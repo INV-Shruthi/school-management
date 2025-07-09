eb973380124202c2fc52e5c6b842532e3f7e8a11
INV-Shruthi <shruthi.m@innovaturelabs.com>
2025-07-09 15:21:40 +0530

    Initial teacher management APIs and JWT setup

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import CustomUser, Teacher, Student
 
-# Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Teacher)
+admin.site.register(Student)
--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
-from .models import Teacher, Student
+from .models import CustomUser, Teacher
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'role']
 
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
