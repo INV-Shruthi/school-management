5f7c78a3c0bf233661f6b8afd181af6dc711f5de
INV-Shruthi <shruthi.m@innovaturelabs.com>
2025-07-16 18:07:55 +0530

    Add password reset functionality using email

--- config/settings.py
+++ config/settings.py
@@ -114,6 +114,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'projectpurpose02@gmail.com'              
+EMAIL_HOST_PASSWORD = 'zmvw yzig mtjc ixtc'       
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TeacherViewSet , StudentViewSet , RegisterUserView ,UserViewSet,import_students_csv
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import CustomTokenView
+from .views import CustomTokenView ,send_reset_email, reset_password
 
 router = DefaultRouter()
 router.register('teachers', TeacherViewSet, basename='teacher')
@@ -16,6 +16,8 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterUserView.as_view(), name='user-register'),
     path('import/students/', import_students_csv),
+    path('reset-password-link/', send_reset_email),
+    path('reset-password/<str:token>/', reset_password),
 
 
 ]
